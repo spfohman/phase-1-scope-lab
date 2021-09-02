5a5123c68364e0f9de0c1229ca0058c2c5c9b4de
Sarah <spfohman@email.essex.edu>
2021-09-02 09:41:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    const name = customerName.toUpperCase();
+}
+var bestCustomer ;
+function setBestCustomer(bestCustomer){
+    var bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(bestCustomer){
+    var bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'rob';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'oscar';
+}
